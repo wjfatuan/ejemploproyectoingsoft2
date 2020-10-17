50fa35711fc72cbdb63e9094c5a78f4c88fb78f6
wjfatuan <wilson.forero@uan.edu.co>
2020-10-17 11:02:15 -0500

    Se agregan algunos modelos a la aplicacion

--- vuelos/admin.py
+++ vuelos/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import Ciudad, Aeropuerto, Vuelo
+
+class CiudadAdmin(admin.ModelAdmin):
+    list_display = ('id','codigo','pais', 'nombre')
+    fields = ('pais','nombre','codigo',)
+
 # Register your models here.
+admin.site.register(Ciudad, CiudadAdmin)
+admin.site.register(Aeropuerto)
+admin.site.register(Vuelo)
--- vuelos/models.py
+++ vuelos/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+class Ciudad(models.Model):
+
+    COUNTRIES = [
+        ('CO', 'Colombia'),
+    ]
+    nombre = models.CharField(max_length=64)
+    pais = models.CharField(max_length=64, choices=COUNTRIES)
+    codigo = models.IntegerField()
+
+    def __str__(self) -> str:
+        return str(self.codigo) + " - " + self.nombre
+
+
+class Aeropuerto(models.Model):
+
+    nombre = models.CharField(max_length=64)
+    codigo = models.CharField(max_length=3)
+    ciudad = models.ForeignKey(Ciudad, on_delete=models.PROTECT)
+
+    def __str__(self) -> str:
+        return str(self.codigo) + " -   " + self.nombre
+
+class Vuelo(models.Model):
+    origen = models.ForeignKey(Aeropuerto, on_delete=models.PROTECT, related_name='origen')
+    destino = models.ForeignKey(Aeropuerto, on_delete=models.PROTECT, related_name='destino')
+    costo = models.FloatField()
+    sillas = models.IntegerField()
+    fecha_hora = models.DateTimeField()
