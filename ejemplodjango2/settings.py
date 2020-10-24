49338792d160deb0399f647f185df7f2fb78dc30
wjfatuan <wilson.forero@uan.edu.co>
2020-10-24 09:49:23 -0500

    Configurar para Heroku

--- ejemplodjango2/settings.py
+++ ejemplodjango2/settings.py
@@ -1,3 +1,5 @@
+import os
+import django_heroku
 """
 Django settings for ejemplodjango2 project.
 
@@ -11,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -119,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+django_heroku.settings(locals())
